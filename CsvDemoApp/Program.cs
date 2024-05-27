9bc6ad001e6f9e34c4a39a9a3ac05532c30e8a7e
Ionut Radu <radutudorion@gmail.com>
2024-05-27 19:09:14 +0300

    Add project files.

---
+++ CsvDemoApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using CsvHelper;
+using System.Globalization;
+
+namespace CsvDemoApp;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string inputFile = @"C:\Users\radut\source\repos\CsvDemoApp\CsvDemoApp";
+        string outputFile = @"C:\Users\radut\source\repos\CsvDemoApp\CsvDemoApp";
+
+        List<CustomerModel> outputRecords = new();
+        using var reader = new StreamReader(inputFile);
+        using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+    }
+}
